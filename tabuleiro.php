3e58a44400cecbc8c4c3381c603a4f25414fb126
Guilherme de Medeiros <103907336+GuiMed18@users.noreply.github.com>
2023-02-13 11:57:33 -0300

    Add files via upload

---
+++ tabuleiro.php
@@ -0,0 +1,21 @@
+<?php
+$casas = rand(0,4);
+
+switch($casas){
+	case 0:
+	echo "Avance 1 casa";
+	break;
+	case 1:
+	echo "Retorne 1 casa";
+	break;
+	case 2:
+	echo "Compre mais uma carta";
+	break;
+	case 3:
+	echo "Avance 2 casas!";
+	break;
+	case 4:
+	echo "Retorne 3 casas!";
+	break;
+	
+}
